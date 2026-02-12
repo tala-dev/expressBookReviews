4fe0da1d918b6d706df9d2a3a1530f48e8398b4f
tala-dev <tlatypova.dev@gmail.com>
2026-02-12 15:28:21 -0500

    Submit 1.0

--- final_project/index.js
+++ final_project/index.js
@@ -3,6 +3,35 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
+const users = require('./router/auth_users.js').users;
+
+// Check if a user with the given username already exists
+const doesExist = (username) => {
+    // Filter the users array for any user with the same username
+    let userswithsamename = users.filter((user) => {
+        return user.username === username;
+    });
+    // Return true if any user with the same username is found, otherwise false
+    if (userswithsamename.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Check if the user with the given username and password exists
+const authenticatedUser = (username, password) => {
+    // Filter the users array for any user with the same username and password
+    let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+    // Return true if any valid user is found, otherwise false
+    if (validusers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 const app = express();
 
@@ -11,7 +40,20 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    if (req.session.authorization) { // Get the authorization object stored in the session
+        token = req.session.authorization['accessToken']; // Retrieve the token from authorization object
+        jwt.verify(token, "access", (err, user) => { // Use JWT to verify token
+            if (!err) {
+                req.user = user;
+                console.log("authenticated");
+                next();
+            } else {
+                return res.status(403).json({ message: "User not authenticated" });
+            }
+        });
+    } else {
+        return res.status(403).json({ message: "User not logged in" });
+    }
 });
  
 const PORT =5000;
